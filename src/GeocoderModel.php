f9839b3a659dc08bcaad37d6657e233493a95d0a
HUSSON Kevin <husson.kevin@gmail.com>
2018-04-04 14:15:40 +0200

    Add getter for a common formatted type

--- src/GeocoderModel.php
+++ src/GeocoderModel.php
@@ -43,4 +43,33 @@ class GeocoderModel
 	 * @var float
 	 */
 	public $score;
+
+	/**
+	 * Return formatted type
+	 * 
+	 * @return string
+	 */
+	public function getType()
+	{
+		$value	= $this->type;
+		switch($this->type){
+			case 'housenumber':
+			case 'yes':
+			case 'street_address':
+				$value	= 'house';
+				break;
+			case 'street':
+			case 'residential':
+			case 'route':
+				$value	= 'road';
+				break;
+			case 'municipality':
+			case 'city':
+			case 'postal_code':
+				$value	= 'city';
+				break;
+		}
+
+		return $value;
+	}
 }
